40cb48933a5bfb5acf385d6752fb2e56705677d2
LUXIANZE <luxianze98@gmail.com>
2020-12-07 09:28:51 +0800

    Clean up mock data and backend dependencies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('http://192.168.1.51:3000/')//use your own pc ip address
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>{item.name}, {item.model}, {item.phoneNo}, {item.plateNumber}</Text>
-          )}
-        />
-      )}
-    </View>
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
   );
 }
 
