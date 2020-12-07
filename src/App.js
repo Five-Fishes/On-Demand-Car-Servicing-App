018145ade78c549a6dda4537e232622b23e2d8ad
LUXIANZE <luxianze98@gmail.com>
2020-12-07 09:50:44 +0800

    Navigation stack setup - Created Stack Container - Created component Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './pages';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
