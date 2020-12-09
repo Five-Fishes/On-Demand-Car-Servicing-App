e62d7aec694f17b3cae69a65958b60038c45801e
LUXIANZE <luxianze98@gmail.com>
2020-12-10 02:11:35 +0800

    Login Page Fixes #20

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen, SignUpScreen } from "./pages";
+import SignInScreen from "./pages/SignIn/SignIn";
 
 const Stack = createStackNavigator();
 
@@ -9,7 +10,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/pages/SignIn/SignInStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+export default SignInStyle = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    width: "100%",
+    flex: 1,
+  },
+  inputBorder: {
+    margin: 10,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 5,
+  },
+  input: {
+    alignSelf: "stretch",
+    padding: 10,
+  },
+});
