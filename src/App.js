d64f27a4351abcc9d759dbb9d55e3b8f238d6006
HowardYaw <yjhhoward@gmail.com>
2020-12-10 13:51:28 +0800

    add page title, remove unsed import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen, SignUpScreen, ProfileScreen } from "./pages";
-// import { ProfileScreen } from "./pages/Profile/Profile";
 
 const Stack = createStackNavigator();
 
@@ -12,7 +11,11 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{title: "Profile"}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
