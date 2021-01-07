602161d7c51e24a7e39cba694fff883b3b8c7cf4
HowardYaw <yjhhoward@gmail.com>
2021-01-08 01:21:22 +0800

    setup bottom nav and stack screen

---
+++ src/navigation/Stack/ProfileStackScreen.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ProfileScreen, SettingsScreen } from "../../pages";
+
+const Stack = createStackNavigator();
+
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Profile"
+      screenOptions={{
+        headerStyle: {
+          elevation: 0,
+          shadowOpacity: 0,
+          backgroundColor: "#1C4687",
+          borderBottomWidth: 0,
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Profile" component={ProfileScreen}
+        options={{ title: "Profile" }}
+      />
+      <Stack.Screen
+        name="Settings" component={SettingsScreen}
+        options={{ title: "Setting" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default ProfileStack;
